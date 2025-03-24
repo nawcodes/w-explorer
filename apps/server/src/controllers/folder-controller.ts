aa2e9d877df4442d69a2005ffbc4b99e049f7870
Nawcodes <nawcodes@gmail.com>
2025-03-24 23:08:00 +0700

    add: test get all folders.

--- apps/server/src/controllers/folder-controller.ts
+++ apps/server/src/controllers/folder-controller.ts
@@ -1,11 +1,12 @@
 import type { Request, Response } from 'express';
 import 'reflect-metadata';
 import { Controller, Param, Body, Get, Post, Put, Delete, JsonController, Res } from 'routing-controllers';
+import { FolderService } from '../services/folder-service';
 
 @JsonController()
 export class FolderController {
     @Get('/folders')
     getAll(@Res() res: Response) {
-        return res.send('All Folders')
+        return new FolderService().getAllFolders()
     }
 }
