4eb842958bb624738a6de88680ee7a23eb752efe
Nawcodes <nawcodes@gmail.com>
2025-03-24 22:10:12 +0700

    add: at index test connections

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
+import { prisma } from './utils/prisma';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from the backend!');
-});
+const testConnection = async () => {
+    try {
+        await prisma.folder.findFirst()
+        console.log('Connected to Prisma ORM')
+    } catch (error) {
+        console.error('Failed to connect to Prisma ORM', error)
+    }
+}
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    await testConnection();
     console.log(`Server is running on http://localhost:${PORT}`);
 });
