f329dabc5fdeaa7441f5deea2ba94353b1ce2f31
Nawcodes <nawcodes@gmail.com>
2025-03-26 14:38:54 +0700

    fix: default home breadcumb

--- apps/client/src/components/content/Breadcrumb.vue
+++ apps/client/src/components/content/Breadcrumb.vue
@@ -3,15 +3,8 @@
     <ol class="inline-flex items-center space-x-1 md:space-x-3">
       <!-- Root Link -->
       <li class="inline-flex items-center">
-        <a href="#" 
-          class="inline-flex items-center text-sm text-gray-700 hover:text-blue-600"
-          @click.prevent="$emit('navigate', null)"
-        >
-          <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-          </svg>
-          Root
-        </a>
+       
+          
       </li>
 
       <!-- Path Items -->
@@ -20,9 +13,13 @@
           <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
         </svg>
         <a href="#" 
-          class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2"
+          class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 inline-flex"
           @click.prevent="$emit('navigate', index)"
         >
+          <!-- index 0 is svg home icon -->
+          <svg v-if="index === 0" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+          </svg>
           {{ crumb }}
         </a>
       </li>
